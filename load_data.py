ad8212817de3182989531e70fe641589bd773e89
须子逸 <41245564+XuZiyi-0@users.noreply.github.com>
2020-12-19 18:40:53 +0800

    增加最大池化与平均池化功能

--- load_data.py
+++ load_data.py
@@ -2,13 +2,15 @@ import os
 from os.path import join as pjoin
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as F
 import cv2
 import re
 
 
 
 #读取文件夹中的数据，如读取train文件夹中的数据
-def load_data_(data_path, class_list, size = 128, augmentate=False):
+def load_data_(data_path, class_list, size = 128, mod='diff', augmentate=True):
     X = []
     Y = []
     train_dirs = os.listdir(data_path)
@@ -27,7 +29,7 @@ def load_data_(data_path, class_list, size = 128, augmentate=False):
                 img_trgt = cv2.imread(pjoin(data_path, type, 'trgt', trgt_imgs[i]))
                 img_temp = cv2.imread(pjoin(data_path, type, 'temp', temp_imgs[i]))
                 #数据预处理
-                x = process(img_trgt, img_temp, size)
+                x = process(img_trgt, img_temp, size, mod)
                 X.append(x)
                 Y.append(y)
                 # f_trgt.close()
@@ -35,10 +37,10 @@ def load_data_(data_path, class_list, size = 128, augmentate=False):
     return np.array(X), np.array(Y)
 
 #读取train和test文件夹中的数据
-def load_data(data_path=r'./data/cutted_data',size = 64, class_list = '[[1], [2], [14]]', augmentate = True):
+def load_data(data_path=r'./data/cutted_data',size = 64, class_list = '[[1], [2], [14]]', mod='diff', augmentate = True):
     class_list_ = eval(class_list)
-    train_X, train_Y = load_data_(pjoin(data_path, 'train'), class_list_, size=size, augmentate = augmentate)
-    test_X, test_Y = load_data_(pjoin(data_path, 'test'), class_list_, size=size, augmentate = augmentate)
+    train_X, train_Y = load_data_(pjoin(data_path, 'train'), class_list_, size=size, mod=mod, augmentate = augmentate)
+    test_X, test_Y = load_data_(pjoin(data_path, 'test'), class_list_, size=size, mod=mod, augmentate = augmentate)
     return train_X, train_Y, test_X, test_Y
 
 
@@ -50,10 +52,49 @@ def resize(img, size):
     img = cv2.resize(img, dsize = (size,size))
     return img
 
-def process(img1,img2, size):
-    img = diff(img1, img2)
-    img = resize(img, size)
+def process(img1,img2, size, mod):
+    shape = np.array(img1).shape
+    if mod == 'avg_pool' or mod == 'max_pool':
+        img1, img2 =\
+            torch.from_numpy(np.array(img1)),\
+            torch.from_numpy(np.array(img2))
+        img1_r, img1_g, img1_b = img1[:,:,0], img1[:,:,1], img1[:,:,2]
+        img2_r, img2_g, img2_b = img2[:,:,0], img2[:,:,1], img2[:,:,2]
+        img1_c = [img1_r, img1_g, img1_b]
+        img2_c = [img2_r, img2_g, img2_b]
+        img_c_ = img1_c + img2_c
+        img_c = []
+
+        for i in img_c_:
+            if mod == 'max_pool':
+                i = F.max_pool2d(i.unsqueeze(0).float(), kernel_size=3, stride=1).numpy()
+            elif mod == 'avg_pool':
+                i  = F.avg_pool2d(i.unsqueeze(0).float(), kernel_size=3, stride=1).numpy()
+            i = i[0,:,:]
+            i  = i[:,:,np.newaxis]
+            img_c.append(i)
+
+
+        img1 = np.concatenate((img_c[0], img_c[1], img_c[2]), axis=2)
+        img2 = np.concatenate((img_c[3], img_c[4], img_c[5]), axis=2)
+        img = img1 - img2
+        # img = np.abs(img)
+        img = resize(img, size)
+
+
+    elif mod == 'diff':
+        img = diff(img1, img2)
+        img = resize(img, size)
     return np.array(img)
 
-
-
+if __name__ == '__main__':
+    class_list = '[[1],[2],[14]]'
+    # class_list = '[[1], [2], [3], [4], [5], [6], [7], [14], [16], [17], [20], [21], [22], [23], [24]]'
+    class_num = len(eval(class_list))
+    img_size = 50
+    train_X, train_Y, test_X, test_Y = load_data(data_path=r'./data/cutted_data', size=img_size, mod='max_pool',
+                                                 class_list=class_list, augmentate=True)
+    np.save('x_train', train_X)
+    np.save('y_train', train_Y)
+    np.save("x_test",test_X)
+    np.save('y_test', test_Y)
